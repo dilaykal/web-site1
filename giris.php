19a1ba2407df2a07b3fab7f70684d25ed3146987
Dilay Kal <104075669+dilaykal@users.noreply.github.com>
2022-08-20 21:48:57 +0300

    giris.php tamamlandı

--- giris.php
+++ giris.php
@@ -1,12 +1,44 @@
 <?php
-        $örmail=$_POST["mail"];
-        $örsifre=$_POST["sifre"];
-        if (isset($_POST['mail'], $_POST['sifre'] ) && ($_POST['mail']!='') && ($_POST['sifre']!='')) {
-            echo "Giriş İşleminiz Yapıldı...";
-            echo "<br>"."Hoşgeldiniz"." ".$_POST['mail'] . "<br>"."Şifreniz:"." " . $_POST['sifre']."</b>";
-        }else{
-        echo"Bilgilerinizi Kontrol Edip Tekrar Giriş Yapın.<br>
-        Eksik Bilgi Girdiniz...";
+    $sifreerr = $emailerr = $boserr="";
+    $sifre = $email = "";
+    $sifreerr="şifreniz uygun formatta değil";
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    if(isset($_POST["mail"])){
+        if (empty($_POST["mail"])) {
+            $boserr = "Mail alanı doldurulmak zorunludur";
+            echo $boserr;
+        } 
+        else {
+            $email = test_input($_POST["mail"]);
+            // check if e-mail address is well-formed
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailerr = "Yanlış e-mail formatı girdiniz";
+            echo $emailerr;
+            }
+            else{
+                echo "Mail adresiniz:"." ".$email;
+            }
         }
-        echo "<p> <a href='login.html'>&lt;GERİ DÖN&gt;</a></p> ";
+        if (empty($_POST["sifre"])) {
+            $sifreerr = "Bu alanı doldurunuz";
+            echo $sifreerr;
+        } 
+        else{
+            $sifre = $_POST["sifre"];
+            $kontrol = "/\S*((?=\S{9,})(?=\S*[a-z]))\S*/";
+            if(preg_match($kontrol,$sifre))
+            {  
+                echo "Şifreniz:"." ".$sifre;
+            }
+            else{
+                echo "Şifreniz uygun formatta değil.";
+            }        
+        }
+    }
+    echo "<p> <a href='login.html'>&lt;GERİ DÖN&gt;</a></p> ";
 ?>
